d70c0dda1284e4a030dc4210eccd41e3b0d942d0
Albert Akrong <alphaxsalt@gmail.com>
2022-06-18 09:46:46 +0000

    Add sidebar icons

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { Box } from "@mui/material";
 import Topbar from "./topbar";
 import Sidebar from "./sidebar";
+import Divider from "@mui/material/Divider";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -9,10 +10,11 @@ type LayoutProps = {
 
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
-    <Box height="100vh" py={0.5} px={1}>
-      <Box>
+    <Box height="100vh">
+      <Box py={0.5} px={1}>
         <Topbar />
       </Box>
+      <Divider />
       <Box sx={{ display: "flex", justifyContent: "flex-start" }}>
         <Box
           sx={{
--- components/layout/sidebar/index.tsx
+++ components/layout/sidebar/index.tsx
@@ -1,7 +1,47 @@
 import { Box } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import InboxIcon from "@mui/icons-material/Inbox";
+import DraftsIcon from "@mui/icons-material/Drafts";
+import ComposeButton from "../../core/ComposeButton";
 
 const Sidebar = () => {
-  return <Box></Box>;
+  return (
+    <Box>
+      <Box mt={1.8}>
+        <ComposeButton>
+          <AddIcon /> Compose
+        </ComposeButton>
+      </Box>
+      <Box>
+        <nav aria-label="main mailbox folders">
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary="Inbox" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <DraftsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Drafts" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </nav>
+      </Box>
+    </Box>
+  );
 };
 
 export default Sidebar;
